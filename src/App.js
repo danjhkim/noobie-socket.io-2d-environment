6aebc32de72d646f190765d3903821c25ddead1b
danjhkim <danjhkim@gmail.com>
2022-02-18 00:40:19 -0500

    working model

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React, { useState, useEffect, useMemo, useRef } from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 
-import { Stage, Layer, Circle, Rect } from 'react-konva';
+import { Stage, Layer, Circle } from 'react-konva';
 import './App.css';
 import socket from './apis/socket.io';
+import useImage from 'use-image';
+import Me from './images/Me.png';
+
 const randomColor = require('randomcolor'); // import the script
 
 function App() {
 	const [isDragging, setIsDragging] = useState(false);
-	const [myCircle, setMyCircle] = useState({ x: 50, y: 50 });
 	const others = useRef();
+	const [image] = useImage('./images/Me.png');
+	const [peopletest, setPeopleTest] = useState();
 
-	socket.on('connection', data => {});
-
-	socket.on('positions', data => {
-		const newListWithoutYou = data.filter(item => {
-			return item.id !== myCircle.id;
+	useEffect(() => {
+		socket.on('positions', data => {
+			console.log('once per movement');
+			const newListWithoutYou = data.filter(item => {
+				return item.id !== socket.id;
+			});
+			others.current = newListWithoutYou;
+			setPeopleTest(newListWithoutYou);
 		});
-		others.current = newListWithoutYou;
-	});
+	}, []);
 
-	const renderOthers = () => {
-		console.log(others);
+	const renderOthers = useCallback(() => {
 		if (Array.isArray(others.current)) {
 			return others.current.map((person, index) => {
-				console.log(person);
 				return (
-					<Rect
+					<Circle
 						key={index}
+						radius={40}
 						id={person.id}
 						x={person.x}
 						y={person.y}
@@ -37,61 +42,52 @@ function App() {
 					/>
 				);
 			});
-
-			//for (let i of array) {
-			// 	return (
-			// 		<Rect
-			// 			id={i.id}
-			// 			x={20}
-			// 			y={20}
-			// 			width={50}
-			// 			height={50}
-			// 			fill='red'
-			// 			shadowBlur={5}
-			// 		/>
-			// 	);
-			// }
 		}
-	};
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [peopletest]);
 
 	return (
 		<div className='App'>
 			<Stage width={window.innerWidth} height={window.innerHeight}>
 				<Layer>
 					{renderOthers()}
-					<Circle
-						radius={40}
-						text='Draggable Text'
-						x={myCircle.x}
-						y={myCircle.y}
-						draggable
-						fill={isDragging ? 'green' : 'black'}
-						onDragStart={() => {
-							setIsDragging(true);
-						}}
-						onMouseEnter={e => {
-							const container = e.target.getStage().container();
-							container.style.cursor = 'pointer';
-						}}
-						onMouseLeave={e => {
-							const container = e.target.getStage().container();
-							container.style.cursor = 'default';
-						}}
-						onDragEnd={e => {
-							setIsDragging(false);
-							setMyCircle({
-								x: e.target.x(),
-								y: e.target.y(),
-								id: socket.id,
-							});
-
-							socket.emit('people', {
-								x: e.target.x(),
-								y: e.target.y(),
-								id: socket.id,
-							});
-						}}
-					/>
+					<>
+						<Circle
+							radius={40}
+							x={50}
+							y={50}
+							draggable
+							fillPatternImage={image}
+							fillPatternRepeat={'no-repeat'}
+							stroke={'1'}
+							strokeWidth={isDragging ? 3 : 1}
+							opacity={0.2}
+							shadowBlur={isDragging ? 0 : 1}
+							onDragStart={() => {
+								setIsDragging(true);
+							}}
+							onMouseEnter={e => {
+								const container = e.target
+									.getStage()
+									.container();
+								container.style.cursor = 'pointer';
+							}}
+							onMouseLeave={e => {
+								const container = e.target
+									.getStage()
+									.container();
+								container.style.cursor = 'default';
+							}}
+							onDragEnd={e => {
+								setIsDragging(false);
+								socket.emit('people', {
+									x: e.target.x(),
+									y: e.target.y(),
+									id: socket.id,
+								});
+							}}
+						/>
+					</>
 				</Layer>
 			</Stage>
 		</div>
